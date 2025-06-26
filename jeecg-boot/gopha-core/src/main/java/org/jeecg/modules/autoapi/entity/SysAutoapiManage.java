9b80b2e5bcc13e9d793f20612b9729945a8eb0d0
cdbmfrcjj <cdbmfrcjj@163.com>
2025-06-26 10:32:55 +0800

    1

--- jeecg-boot/gopha-core/src/main/java/org/jeecg/modules/autoapi/entity/SysAutoapiManage.java
+++ jeecg-boot/gopha-core/src/main/java/org/jeecg/modules/autoapi/entity/SysAutoapiManage.java
@@ -56,42 +56,42 @@ public class SysAutoapiManage implements Serializable {
 	/**名称*/
 	@Excel(name = "名称", width = 15)
     @Schema(description = "名称")
-    @TableField(updateStrategy = FieldStrategy.IGNORED)
+    @TableField(updateStrategy = FieldStrategy.ALWAYS)
     private String name;
 	/**描述*/
 	@Excel(name = "描述", width = 15)
     @Schema(description = "描述")
-    @TableField(updateStrategy = FieldStrategy.IGNORED)
+    @TableField(updateStrategy = FieldStrategy.ALWAYS)
     private String description;
 	/**参数描述*/
 	@Excel(name = "参数描述", width = 15)
     @Schema(description = "参数描述")
-    @TableField(updateStrategy = FieldStrategy.IGNORED)
+    @TableField(updateStrategy = FieldStrategy.ALWAYS)
     private String paramsDescription;
 	/**执行参数*/
 	@Excel(name = "执行参数", width = 15)
     @Schema(description = "执行参数")
-    @TableField(updateStrategy = FieldStrategy.IGNORED)
+    @TableField(updateStrategy = FieldStrategy.ALWAYS)
     private String doParams;
 	/**身份验证*/
 	@Excel(name = "身份验证", width = 15, dicCode = "autoapiPermissions")
     @Schema(description = "身份验证")
-    @TableField(updateStrategy = FieldStrategy.IGNORED)
+    @TableField(updateStrategy = FieldStrategy.ALWAYS)
     private String permission;
     /**执行日志*/
 	@Excel(name = "执行日志", width = 15, dicCode = "autoapiPermissions")
     @Schema(description = "执行日志")
-    @TableField(updateStrategy = FieldStrategy.IGNORED)
+    @TableField(updateStrategy = FieldStrategy.ALWAYS)
     private String saveLog;
 	/**通用处理*/
 	@Excel(name = "通用处理", width = 15, dictTable = "sys_autoapi_handle", dicText = "name", dicCode = "id")
 	@Dict(dictTable = "sys_autoapi_handle", dicText = "name", dicCode = "id")
     @Schema(description = "通用处理")
-    @TableField(updateStrategy = FieldStrategy.IGNORED)
+    @TableField(updateStrategy = FieldStrategy.ALWAYS)
     private String handleId;
 	/**代码*/
 	@Excel(name = "代码", width = 15)
     @Schema(description = "代码")
-    @TableField(updateStrategy = FieldStrategy.IGNORED)
+    @TableField(updateStrategy = FieldStrategy.ALWAYS)
     private String codeText;
 }
--- jeecgboot-vue3/src/views/gophacore/autoapi/components/SysAutoapiAddressModal.vue
+++ jeecgboot-vue3/src/views/gophacore/autoapi/components/SysAutoapiAddressModal.vue
@@ -1,13 +1,24 @@
 <template>
-  <a-modal :title="title" :width="width" :open="visible" :confirmLoading="confirmLoading" @cancel="handleCancel"
-    :destroyOnClose="true" :afterClose="reset" cancelText="关闭">
+  <a-modal
+    :title="title"
+    :width="width"
+    :open="visible"
+    :confirmLoading="confirmLoading"
+    @cancel="handleCancel"
+    :destroyOnClose="true"
+    :afterClose="reset"
+    cancelText="关闭"
+  >
     <a-spin :spinning="confirmLoading">
-
-      <a-form :form="formRef" class="antd-modal-form" :labelCol="{ xs: { span: 24 }, sm: { span: 5 } }"
-        :wrapperCol="{ xs: { span: 24 }, sm: { span: 16 } }">
+      <a-form
+        :form="formRef"
+        class="antd-modal-form"
+        :labelCol="{ xs: { span: 24 }, sm: { span: 5 } }"
+        :wrapperCol="{ xs: { span: 24 }, sm: { span: 16 } }"
+      >
         <a-row>
           <a-col :span="24">
-            <a-form-item label="地址" style="padding-top: 25px;">
+            <a-form-item label="地址" style="padding-top: 25px">
               <div class="flex justify-center">
                 <a-input v-model:value="url" />
                 <a-button type="primary" @click="handleCopy"> 复制 </a-button>
@@ -17,7 +28,6 @@
           </a-col>
         </a-row>
       </a-form>
-
     </a-spin>
     <template #footer>
       <a-button @click="handleCancel">关闭</a-button>
@@ -26,46 +36,46 @@
 </template>
 
 <script lang="ts" setup>
-import { ref,unref } from 'vue';
-import { Form } from 'ant-design-vue';
-import { useMessage } from '/@/hooks/web/useMessage';
-import { useCopyToClipboard } from '/@/hooks/web/useCopyToClipboard';
-const formRef = ref();
-const useForm = Form.useForm;
-const title = ref("调用地址");
-const width = ref(750);
-const visible = ref(false);
-const url = ref("");
-const confirmLoading = ref<boolean>(false);
-const { createMessage } = useMessage();
-const { clipboardRef, copiedRef } = useCopyToClipboard();
+  import { ref, unref } from 'vue';
+  import { Form } from 'ant-design-vue';
+  import { useMessage } from '/@/hooks/web/useMessage';
+  import { useCopyToClipboard } from '/@/hooks/web/useCopyToClipboard';
+  const formRef = ref();
+  const useForm = Form.useForm;
+  const title = ref('调用地址');
+  const width = ref(750);
+  const visible = ref(false);
+  const url = ref('');
+  const confirmLoading = ref<boolean>(false);
+  const { createMessage } = useMessage();
+  const { clipboardRef, copiedRef } = useCopyToClipboard();
 
-function open(recordparams) {
-  url.value = window._CONFIG['domianURL'] + "/autoApi/execute/" + (recordparams.permission == "disable" ? "public" : "private") + "/" + recordparams.name
-  visible.value = true;
-}
-function handleCopy() {
-  if (!url.value) {
-    createMessage.warning('请输入要拷贝的内容！');
-    return;
+  function open(recordparams) {
+    url.value =
+      window._CONFIG['domianURL'] + '/autoApi/execute/' + (recordparams.permission == 'disable' ? 'public' : 'private') + '/' + recordparams.name;
+    visible.value = true;
   }
-  clipboardRef.value = url.value;
-  if (unref(copiedRef)) {
-    createMessage.warning('复制成功');
+  function handleCopy() {
+    if (!url.value) {
+      createMessage.warning('请输入要拷贝的内容！');
+      return;
+    }
+    clipboardRef.value = url.value;
+    if (unref(copiedRef)) {
+      createMessage.warning('复制成功');
+    }
   }
-}
-function handleCancel() {
-  close();
-}
-function close() {
-  visible.value = false;
-}
-function reset() {
-}
+  function handleCancel() {
+    close();
+  }
+  function close() {
+    visible.value = false;
+  }
+  function reset() {}
 
-defineExpose({
-  open
-});
+  defineExpose({
+    open,
+  });
 </script>
 
 <style></style>
